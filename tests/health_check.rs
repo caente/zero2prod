98ea9a77d363dfdd845450ec0d33ac1ce27c5f98
Miguel Iglesias <miguel.iglesias@dimeder.com>
2025-12-23 14:42:55 -0500

    config!

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,7 @@
 use reqwest;
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::test]
@@ -19,6 +21,11 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_200_for_valid_form_data() {
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read conf");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Connection failed");
     let client = reqwest::Client::new();
     let body = "name=le%20guin&email=ursula_le_guin%30gmail.com";
     let response = client
@@ -29,6 +36,12 @@ async fn subscribe_returns_200_for_valid_form_data() {
         .await
         .expect("Failed to execute request");
     assert_eq!(200, response.status().as_u16());
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch");
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
