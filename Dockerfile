2f004822396b5f17dd1f0117716c011401df50dc
Miguel Iglesias <miguel.iglesias@dimeder.com>
2025-12-30 09:33:29 -0500

    Dockerfile small

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,18 @@
-FROM rust:1.92.0-alpine
+FROM rust:1.92.0-alpine as builder
 WORKDIR /app
 RUN apk update && apk add lld clang
-ENV SQLX_OFFLINE true
 COPY . .
+ENV SQLX_OFFLINE=true
 RUN cargo build --release
-ENTRYPOINT ["./target/release/zero2prod"]
+
+FROM debian:bookworm-slim as runtime
+WORKDIR /app
+RUN apt-get update -y \
+    && apt-get upgrade -y --no-install-recommends openssl ca-certificates \
+    && apt-get autoremove -y \
+    && apt-get clean -y \
+    && rm -rf /var/lib/lists/*
+COPY --from=builder /app/target/release/zero2prod zero2prod
+COPY configuration configuration
+ENV APP_ENV=production
+ENTRYPOINT ["./zero2prod"]
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use secrecy::ExposeSecret;
 use sqlx::{Connection, PgPool};
 use std::net::TcpListener;
-use secrecy::ExposeSecret;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry;
@@ -10,10 +10,13 @@ async fn main() -> std::io::Result<()> {
     let subscriber = telemetry::get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     telemetry::init_subscriber(subscriber);
     let configuration = get_configuration().expect("Missing conf file");
-    let connection = PgPool::connect(&configuration.database.connection_string().expose_secret())
-        .await
-        .expect("Failed to read configuration");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let connection =
+        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
+            .expect("Failed to read configuration");
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
     run(listener, connection)?.await
 }
